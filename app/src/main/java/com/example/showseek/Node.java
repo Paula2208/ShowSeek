5aa466ccb9a486d3255d7d16232c834227ebcd86
Paula2208 <pguzmanz@unal.edu.co>
2021-11-21 20:53:15 -0500

    Clase nodo para datos genéricos.

--- app/src/main/java/com/example/showseek/Node.java
+++ app/src/main/java/com/example/showseek/Node.java
@@ -1,24 +1,34 @@
 package com.example.showseek;
 
 public class Node {
-    private int data;
+    //Atributes
+    private Object data;
     private Node next;
+
+    //Constructors
     public Node(){
         this(0);
     }
-    public Node(int data) {
+    public Node(Object data) {
         this.data = data;
         next = null;
     }
-    public int getData() {
+
+    //Methods
+        //Return the info in the Node
+    public Object getData() {
         return data;
     }
-    public void setData(int data) {
+        //Set the info in the Node
+    public void setData(Object data) {
         this.data = data;
     }
+
+    //Next Node associated
     public Node getNext() {
         return next;
     }
+
     public void setNext(Node next) {
         this.next = next;
     }
