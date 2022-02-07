56ba45797e01d618ccab5bc821c8480feaa8baf4
Paula2208 <pguzmanz@unal.edu.co>
2022-02-06 21:04:08 -0500

    Nuevos

---
+++ app/src/main/java/com/example/showseek/layout/CreacionContrato.java
@@ -0,0 +1,16 @@
+package com.example.showseek.layout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.showseek.R;
+
+public class CreacionContrato extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_creacion_contrato);
+    }
+}
---
+++ app/src/main/java/com/example/showseek/layout/VistaFacturacion.java
@@ -0,0 +1,16 @@
+package com.example.showseek.layout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.showseek.R;
+
+public class VistaFacturacion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vista_facturacion);
+    }
+}
